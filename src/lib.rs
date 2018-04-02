0c049da2c2ede7ed7dce80391c6da6579cc0ad2d
Patrick Loughney <patrickloughney0@gmail.com>
2018-04-02 18:00:57 -0400

    Move to static dispatch (NB. Stream doesn't compile yet)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 use std::collections::HashMap;
 use std::cell::Cell;
-use std::fmt::Debug;
-use std::any::Any;
 
-trait Walk : Debug {
-    fn walk(&self, &SMap) -> Box<Walk>;
-    fn as_any(&self) -> &Any;
+trait Walk<T: Walk<T>> {
+    type Output;
+    fn walk(&self, &SMap<T>) -> Self::Output;
 }
 
-struct SMap(HashMap<LVar, Box<Walk>>);
+trait Unify<T: Walk<T>> {
+    fn unify(&self, T, SMap<T>) -> Option<SMap<T>>;
+}
+
+struct SMap<T: Walk<T>>(HashMap<LVar, T>);
+
+impl<'a> SMap<LVal<'a>> {
+    fn add(&mut self, var: LVal, val: LVal<'a>) {
+        if let LVal::LVar(lvar) = var {
+            self.0.insert(lvar, val);
+        } else {
+            panic!("Key of an SMap needs to be an LVar");
+        }
+    }
+}
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct LVar {
@@ -24,35 +36,83 @@ impl LVar {
     }
 }
 
-impl Walk for LVar {
-    fn walk(&self, map: &SMap) -> Box<Walk> {
-        if let Some(var) = map.0.get(self) {
-            var.walk(map)
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+pub enum LVal<'a> {
+    LVar(LVar),
+    GInt(u64),
+    GStr(&'a str),
+    Nil,
+}
+
+impl<'a> LVal<'a> {
+    fn is_lvar(&self) -> bool {
+        if let LVal::LVar(_) = *self {
+            true
         } else {
-            Box::new(*self)
+            false
         }
     }
-    fn as_any(&self) -> &Any { self }
 }
 
-macro_rules! opaque_impl_walk {
-    ($type:ty) =>
-        (
-            impl Walk for $type {
-                fn walk(&self, _: &SMap) -> Box<Walk> {
-                    Box::new(*self)
+impl<'a> Walk<LVal<'a>> for LVal<'a> {
+    type Output = LVal<'a>;
+    fn walk(&self, map: &SMap<LVal<'a>>) -> LVal<'a> {
+        match *self {
+            LVal::LVar(lvar) => 
+                if let Some(var) = map.0.get(&lvar) {
+                    var.walk(map)
+                } else {
+                    LVal::LVar(lvar)
                 }
-                fn as_any(&self) -> &Any { self }
-            }
-        );
-    ($type:ty, $($types:ty),+) =>
-        (
-            opaque_impl_walk!($type);
-            opaque_impl_walk!($($types),+);
-        )
+            LVal::GInt(i) => LVal::GInt(i),
+            LVal::GStr(s) => LVal::GStr(s),
+            LVal::Nil => LVal::Nil
+        }
+    }
 }
 
-opaque_impl_walk!(u8, u16, u32, u64, i8, i16, i32, i64);
+impl<'a> Unify<LVal<'a>> for LVal<'a> {
+    fn unify(&self, other: LVal<'a>, mut map: SMap<LVal<'a>>) -> Option<SMap<LVal<'a>>> {
+        let u = other.walk(&map);
+        let v = self.walk(&map);
+        if u == v {
+            Some(map)
+        } else if u.is_lvar() {
+            map.add(u, v);
+            Some(map)
+        } else if v.is_lvar() {
+            map.add(v, u);
+            Some(map)
+        } else {
+            None
+        }
+    }
+}
+
+pub enum Stream<'a> {
+    Empty,
+    Mature {
+        head: LVal<'a>,
+        next: Box<Stream<'a>>,
+    },
+    Immature {
+        thunk: Box<Fn() -> Stream<'a>>,
+    },
+}
+
+impl<'a> Iterator for Stream<'a> {
+    type Item = LVal<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Stream::Empty => None,
+            Stream::Mature { head, next } => {
+                self = next;
+                Some(*head)
+            },
+            Stream::Immature { thunk } => thunk().next(),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -63,8 +123,8 @@ mod tests {
         let mut s_map = SMap(HashMap::new());
         let l1 = LVar::new();
         let l2 = LVar::new();
-        s_map.0.insert(l1, Box::new(l2));
-        s_map.0.insert(l2, Box::new(0_u32));
-        assert_eq!(Some(&0), l1.walk(&s_map).as_any().downcast_ref::<u32>());
+        s_map.0.insert(l1, LVal::LVar(l2));
+        s_map.0.insert(l2, LVal::GStr("Hello"));
+        assert_eq!(LVal::GStr("Hello"), LVal::LVar(l1).walk(&s_map))
     }
 }
